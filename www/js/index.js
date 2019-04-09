494077c9ab86e00a38e1ebb0e53bf4f39617b032
johnveloper <johnveloper@gmail.com>
2019-04-09 21:18:41 +0200

    Remove default, unecessary html, css and javascript

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,9 @@
 var app = {
-  initialize: function() {
-    this.bindEvents();
-  },
-  bindEvents: function() {
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.    
+  init: function() {
+    // Bind any events that are required on startup. Common events are: load', 'deviceready', 'offline', and 'online'.
     document.addEventListener('deviceready', this.onDeviceReady, false);
   },
   onDeviceReady: function() {
-    // deviceready Event Handler
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'    
-    app.receivedEvent('deviceready');
-  },
-  receivedEvent: function(id) {
-    // Update DOM on a Received Event
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
-
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-
-    console.log('Received Event: ' + id);
+    console.log('Device is ready!');
   }
 };
