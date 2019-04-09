26cd89e9f8eeb738fa5514971a52ab91e082b61d
johnveloper <johnveloper@gmail.com>
2019-04-09 21:28:22 +0200

    Replace 'var' with 'let'

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-var app = {
+let app = {
   init: function() {
     // Bind any events that are required on startup. Common events are: load', 'deviceready', 'offline', and 'online'.
     document.addEventListener('deviceready', this.onDeviceReady, false);
